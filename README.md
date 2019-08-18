e39edba2bd9d29ee5a279f56c4923c1293ca02d5
dlstjq7685 <dlstjq7685@gmail.com>
2019-08-18 09:34:56 +0900

    prepare proj

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # myTask
 electron + react
+
+# Dev env run
+
+yarn start   
+yarn electron-dev    
+   
+---
+# Build
+
+yarn build   
+- react build
+
+---
+
+# release package
+
+yarn pacakge
+  - current target: windows x64
+  - Must react build before package
---
+++ app/src/components/Testconpo.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Testcom extends Component {
+
+    constructor(props) {
+      super(props);
+      
+    }
+  
+    render() {
+        
+        return (
+            <div>
+                <span>
+                    test render
+                </span>
+            </div>
+        );
+    }
+  }
+  
+  export default Testcom;
